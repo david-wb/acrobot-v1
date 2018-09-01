400f51bd99ee2c70ff3c17123b12684ee5f695dc
David Brown <david.brown@lifeomic.com>
2018-09-01 13:08:11 -0600

    reinforce alg

---
+++ policy.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+torch.manual_seed(0) # set random seed
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Policy(nn.Module):
+    def __init__(self, s_size=6, h_size=16, a_size=3):
+        super(Policy, self).__init__()
+        self.fc1 = nn.Linear(s_size, h_size)
+        self.fc2 = nn.Linear(h_size, h_size)
+        self.fc3 = nn.Linear(h_size, a_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.softmax(x, dim=1)
+    
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        probs = self.forward(state).cpu()
+        m = Categorical(probs)
+        action = m.sample()
+        return action.item() - 1, m.log_prob(action)
